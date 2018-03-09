75b2f07a60f28dbbf6a88757e32e5d79b865a01d
Patrick <pkwu@berkeley.edu>
2018-03-09 15:33:41 -0800

    [Add] - Adds api boilerplate

---
+++ boilerplate-api/rest-server/src/config/database/setup.js
@@ -0,0 +1,12 @@
+import {
+  createDatabase,
+  dropDatabase
+} from '../../lib/SQL';
+
+const setup = async () => {
+  // await dropDatabase();
+  // await createDatabase();
+  process.exit();
+};
+
+setup();
