34c13dc3fc9bee21466ef73e9e2b250c3a03d55e
Patrick <pkwu@berkeley.edu>
2018-03-13 09:38:52 -0700

    [Add] - Adds mySQL connection

--- boilerplate-api/rest-server/src/config/database/index.js
+++ boilerplate-api/rest-server/src/config/database/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-import { Pool } from 'pg';
+import mysql from 'mysql';
 import Promise from 'bluebird';
 
 import {
@@ -12,15 +12,13 @@ const config = {
   user: process.env.NODE_ENV === 'production' ? process.env.AWS_USER : process.env.LOCAL_USER,
   host: process.env.NODE_ENV === 'production' ? process.env.AWS_HOST : process.env.LOCAL_HOST,
   database: process.env.NODE_ENV === 'production' ? process.env.AWS_DATABASE : process.env.LOCAL_DATABASE,
-  password: process.env.NODE_ENV === 'production' ? process.env.AWS_PASSWORD : process.env.LOCAL_PASSWORD,
-  port: process.env.NODE_ENV === 'production' ? process.env.AWS_PORT : process.env.LOCAL_PORT,
-  max: 20
+  password: process.env.NODE_ENV === 'production' ? process.env.AWS_PASSWORD : process.env.LOCAL_PASSWORD
 };
 
-const db = new Pool(config);
+const db = mysql.createConnection(config);
 
 db.on('connect', () => {
-  success(`successfully connected to pg ${config.database}`);
+  success(`successfully connected to mysql ${config.database}`);
 });
 
 db.on('remove', client => {
@@ -33,6 +31,12 @@ db.on('error', err => {
 
 db.connect();
 
+
 Promise.promisifyAll(db);
+
+db.queryAsync('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('mySQL queries connected: ', results[0].solution === 2);
+});
 
 export default db;
